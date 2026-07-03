40af01d2d25ba11385556df2d31727b099a26c06
MoisesUrrutia <moisesurrutia49@gmail.com>
2026-07-03 17:29:26 -0500

    add calculator to ask for user name and welcome them

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello, World!\n";
+    std::cout << "Welcome to the C++ Calculator!" << std::endl;
+    std::cout << "What is your name? ";
+    std::string name;
+    std::cin >> name;
+    std::cout << "Hello, " << name << std::endl;
     return 0;
 }
